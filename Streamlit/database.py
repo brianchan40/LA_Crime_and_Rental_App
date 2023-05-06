b250c7af6769cbf41965262e552912e068693ec5
brianchan40 <brian.kihon.chan@gmail.com>
2023-05-06 13:37:23 -0700

    Updated database code to return data from year 2023

--- Streamlit/database.py
+++ Streamlit/database.py
@@ -2,6 +2,13 @@ import pandas as pd
 
 def query_db():
     
-    crime_data = pd.read_csv("../Crime_Data/Processed_Crime_Index/processed_2020_to_Present.csv")
+    crime_data = pd.read_csv("../Crime_Data/Processed_Crime_Index/processed_2010_to_Present.csv")
+    
+    return crime_data[crime_data['year'] == 2023].copy()
     
     
+    
+if __name__ == '__main__':
+    import sys
+    
+    print(query_db())
