558b3a0549542ecec9ea45c162374e61abfeb546
brianchan40 <brian.kihon.chan@gmail.com>
2023-05-24 10:11:41 -0700

    updated database.py for new location

--- Streamlit/database.py
+++ Streamlit/database.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 def query_db():
     
-    crime_data = pd.read_csv("../Crime_Data/Processed_Crime_Index/processed_2010_to_Present.csv")
+    crime_data = pd.read_csv("./data_used/processed_2010_to_Present.csv")
     
     return crime_data[crime_data['year'] == 2023].copy()
     
 def query_zillow():
-    rental_zipcode = pd.read_csv("../Rental_Data/Zillow/Zip_zori_sm_month.csv")
+    rental_zipcode = pd.read_csv("./data_used/Zip_zori_sm_month.csv")
     
     return rental_zipcode[["RegionName", "2023-03-31"]].rename(columns={"RegionName": "neighborhood", "2023-03-31": "zillow_index"}).copy()
 
