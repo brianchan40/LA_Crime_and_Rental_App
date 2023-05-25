b8418bb4c12eaaf5ac62bbbf74e08a63ac7466e2
brianchan40 <brian.kihon.chan@gmail.com>
2023-05-24 19:03:28 -0700

    added a contact page

---
+++ Streamlit/pages/2_Contact.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('Contact Info')
+
+st.markdown(
+        """
+        My name is Brian, and I am a PhD student at UCLA studying Physics, about to graduate this summer (2023 Aug)! I'm currently looking into switching into Data Science, and this is my capstone project as being a part of [The Data Incubator Data Science Fellowship Program](https://www.thedataincubator.com/programs/data-science-bootcamp/). \n
+        If this web application seems interesting to you, if you have any suggestions to improve it, or simply want to connect, feel free to reach out to me through the following! \n\n
+        
+        Email: brian.chan@mg.thedataincubator.com \n
+        LinkedIn: [Brian Chan](https://www.linkedin.com/in/brian-chan-18677524b/) \n
+        
+        And the source code for this particular project can be found [here](https://github.com/brianchan40/LA_Crime_and_Rental_App).
+        """
+    )
